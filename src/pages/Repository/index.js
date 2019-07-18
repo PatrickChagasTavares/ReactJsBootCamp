8c6e8fd24d31b49f457fb8bdeeeeac13561a4fcf
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-18 17:42:47 -0300

    update: defining route to page 'Repository' and searching data from GitHub

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
+// import { Title } from './styles';
 
-import { Title } from './styles';
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
-export default function Repository() {
-  return <Title>Page Repository</Title>;
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+      {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      },
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: true,
+    });
+  }
+
+  render() {
+    const {repository, issues, loading}
+
+    return <h1>Repository</h1>;
+  }
 }
